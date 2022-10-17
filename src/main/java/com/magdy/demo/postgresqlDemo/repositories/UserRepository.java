831985aab2c085abf03598cddbfd0a432e611d0c
magdaly.santos <magdaly1201@gmail.com>
2022-10-17 02:19:25 -0300

    delete el repository

--- src/main/java/com/magdy/demo/postgresqlDemo/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.magdy.demo.postgresqlDemo.repositories;
-
-import com.magdy.demo.postgresqlDemo.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
