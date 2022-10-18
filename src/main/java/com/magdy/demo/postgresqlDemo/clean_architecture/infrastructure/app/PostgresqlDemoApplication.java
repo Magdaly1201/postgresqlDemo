c922db44c9cbe3b3ee7954eba39b51f0a09b04be
magdaly.santos <magdaly1201@gmail.com>
2022-10-18 03:06:42 -0300

    modify run applications

--- src/main/java/com/magdy/demo/postgresqlDemo/clean_architecture/infrastructure/app/PostgresqlDemoApplication.java
+++ src/main/java/com/magdy/demo/postgresqlDemo/clean_architecture/infrastructure/app/PostgresqlDemoApplication.java
@@ -1,4 +1,4 @@
-package com.magdy.demo.postgresqlDemo.clean_architecture.infrastructure.repository.app;
+package com.magdy.demo.postgresqlDemo.clean_architecture.infrastructure.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@SpringBootApplication
+@ComponentScan(basePackages = "com.magdy.demo.postgresqlDemo.clean_architecture")
+@EnableJpaRepositories(basePackages = "com.magdy.demo.postgresqlDemo.clean_architecture.infrastructure")
+@EntityScan("com.magdy.demo.postgresqlDemo.clean_architecture.infrastructure.repository")
 
 public class PostgresqlDemoApplication {
 
