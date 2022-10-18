a2e0b1b9eb35e8fe6d7773db78415dae1ca5909e
magdaly.santos <magdaly1201@gmail.com>
2022-10-18 02:45:02 -0300

    modify package

--- src/main/java/com/magdy/demo/postgresqlDemo/clean_architecture/infrastructure/app/config/SpringDependencyConfig.java
+++ src/main/java/com/magdy/demo/postgresqlDemo/clean_architecture/infrastructure/app/config/SpringDependencyConfig.java
@@ -1,7 +1,9 @@
-package com.magdy.demo.postgresqlDemo.clean_architecture.infrastructure.repository.app.config;
+package com.magdy.demo.postgresqlDemo.clean_architecture.infrastructure.app.config;
 
+import com.magdy.demo.postgresqlDemo.clean_architecture.adapter.delivery.UserEnpoints;
 import com.magdy.demo.postgresqlDemo.clean_architecture.adapter.repository.UserAdapterRepository;
 import com.magdy.demo.postgresqlDemo.clean_architecture.infrastructure.repository.dao.UserDao;
+import com.magdy.demo.postgresqlDemo.services.UserServices;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,4 +23,13 @@ public class SpringDependencyConfig {
         return new UserAdapterRepository(userDao);
     }
 
+    @Bean
+    public UserServices userServices(){
+        return new UserServices(userRepository());
+    }
+
+    @Bean
+    public UserEnpoints userController(){
+        return new UserEnpoints(userServices());
+    }
 }
